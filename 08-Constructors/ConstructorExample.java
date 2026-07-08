20ab0343311698a9b3026eb570aa9a5513e37a22
Sumit <25413sumit@gmail.com>
2026-07-08 12:41:54 +0530

    Renamed folder to improve repository naming consistency

--- 08-Constructors/ConstructorExample.java
+++ 08-Constructors/ConstructorExample.java
@@ -1,6 +1,7 @@
 public class ConstructorExample {
   public static void main(String[] args) {
       Human h1 = new Human(21);
+      Human h2 = new Human();
       System.out.println(h1.age);
 
   }
@@ -8,10 +9,15 @@ public class ConstructorExample {
 
 class Human{
   int age;
+  String name;
+
+  //Default Constructor
   public Human(){
-    System.out.println("IN Constructor");
+    this.name = "Sumit";
+    this.age = 21;
   }
 
+  //Parameterized Constructor
   public Human(int age){
     this.age = age;
   }
